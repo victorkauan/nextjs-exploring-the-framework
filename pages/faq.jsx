2e095553a8cb1e8c08850779947d709eea45eb40
Victor Kauan <victor.oliveira@rodobank.com.br>
2024-05-08 08:03:25 -0300

    :sparkles: feat: add Next link to home and FAQ pages

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function FAQPage() {
   return (
     <div>
       <h1>Alura Cases - FAQ</h1>
-      <a href="/">Go to home</a>
+      <Link href="/">Go to home</Link>
     </div>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
       <h1>Alura Cases - Home</h1>
-      <a href="/faq">Go to FAQ</a>
+      <Link href="/faq">Go to FAQ</Link>
     </div>
   );
 }
