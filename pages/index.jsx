8258023c4ca5671d4435d622362fa3f209d940cd
Victor Kauan <victor.oliveira@rodobank.com.br>
2024-05-10 15:34:37 -0300

    :sparkles: feat: create title component using CSS-in-JS

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,24 @@
 import Link from "next/link";
 
+function Title({ children, as }) {
+  const Tag = as;
+  return (
+    <>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          font-family: sans-serif;
+        }
+      `}</style>
+    </>
+  );
+}
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home</h1>
+      <Title as="h1">Alura Cases - Home</Title>
       <Link href="/faq">Go to FAQ</Link>
     </div>
   );
