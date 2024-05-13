327388623d7623c725a86c93a038227d557b12a3
Victor Kauan <victor.oliveira@rodobank.com.br>
2024-05-12 22:32:34 -0300

    :sparkles: feat: add Open Sans font

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,9 +1,10 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const FAQ_API_URL =
   "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const faq = await fetch(FAQ_API_URL)
     .then((response) => response.json())
     .then((data) => data);
@@ -14,6 +15,9 @@ export async function getStaticProps() {
 export default function FAQPage({ anything, faq }) {
   return (
     <div>
+      <Head>
+        <title>FAQ - Alura Cases Campaign</title>
+      </Head>
       <h1>Alura Cases - FAQ</h1>
       <Link href="/">Go to home</Link>
 
