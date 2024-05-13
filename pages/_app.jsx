92bff83249cf8313ef204cdd545f76cd9d7e076b
Victor Kauan <victor.oliveira@rodobank.com.br>
2024-05-12 22:45:58 -0300

    :sparkles: feat: add Google Analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,22 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HVGMYR8268"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'G-HVGMYR8268');
+            `,
+          }}
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
