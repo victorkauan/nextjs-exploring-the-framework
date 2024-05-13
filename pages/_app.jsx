6f04ec5f3c68d3e0b2a795c84cce8712795faa5e
Victor Kauan <victor.oliveira@rodobank.com.br>
2024-05-12 22:08:05 -0300

    :sparkles: feat: get FAQ data using getStaticProps

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import GlobalStyle from "../src/theme/GlobalStyle";
 
-export default function MyApp({ Component, ...pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,10 +1,32 @@
 import Link from "next/link";
 
-export default function FAQPage() {
+const FAQ_API_URL =
+  "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+
+export async function getStaticProps() {
+  const faq = await fetch(FAQ_API_URL)
+    .then((response) => response.json())
+    .then((data) => data);
+
+  return { props: { anything: "Hello, world", faq } };
+}
+
+export default function FAQPage({ anything, faq }) {
   return (
     <div>
       <h1>Alura Cases - FAQ</h1>
       <Link href="/">Go to home</Link>
+
+      <ul>
+        {faq.map(({ question, answer }) => (
+          <li key={question}>
+            <article>
+              <h2>{question}</h2>
+              <p>{answer}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
