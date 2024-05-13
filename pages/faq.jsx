4ac91e16d295d6d00e025e7c494c6bd0d8e1e21e
Victor Kauan <victor.oliveira@rodobank.com.br>
2024-05-12 23:00:38 -0300

    :sparkles: feat: style FAQ screen

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,8 +1,9 @@
-import Link from "next/link";
-import PageTitle from "../src/components/PageTitle";
+import FAQScreen from "../src/screens/FAQScreen";
 
 const FAQ_API_URL =
   "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+
+export default FAQScreen;
 
 export async function getServerSideProps() {
   const faq = await fetch(FAQ_API_URL)
@@ -10,25 +11,4 @@ export async function getServerSideProps() {
     .then((data) => data);
 
   return { props: { anything: "Hello, world", faq } };
-}
-
-export default function FAQPage({ anything, faq }) {
-  return (
-    <div>
-      <PageTitle>FAQ - Alura Cases Campaign</PageTitle>
-      <h1>Alura Cases - FAQ</h1>
-      <Link href="/">Go to home</Link>
-
-      <ul>
-        {faq.map(({ question, answer }) => (
-          <li key={question}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 }
