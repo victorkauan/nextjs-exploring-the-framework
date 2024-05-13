61adf434a1eb249ad51da022bf08630565b67cce
Victor Kauan <victor.oliveira@rodobank.com.br>
2024-05-12 21:29:07 -0300

    :sparkles: feat: create home page screen

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,3 @@
-import Link from "next/link";
+import HomeScreen from "../src/screens/HomeScreen";
 
-function Title({ children, as }) {
-  const Tag = as;
-  return (
-    <>
-      <Tag>{children}</Tag>
-      <style jsx>{`
-        ${Tag} {
-          color: red;
-        }
-      `}</style>
-    </>
-  );
-}
-
-export default function HomePage() {
-  return (
-    <div>
-      <Title as="h1">Alura Cases - Home</Title>
-      <Link href="/faq">Go to FAQ</Link>
-    </div>
-  );
-}
+export default HomeScreen;
